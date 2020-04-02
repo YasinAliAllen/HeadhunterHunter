4a86983181b3086e0974584aeda87b8b5311dffa
YasinAliAllen <yasin.aliallen@my.jcu.edu.au>
2020-04-03 00:24:55 +1100

    clean code

--- app/src/main/java/au/edu/jcu/cp3406/headhunterhunter/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/headhunterhunter/MainActivity.java
@@ -36,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         if (savedInstanceState != null) {
             itemName = savedInstanceState.getString("itemName");
         }
